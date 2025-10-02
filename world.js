f76debb6d604857cae713a488bec812992fd4d6c
JustSomeGuy420 <alwynsterling@gmail.com>
2025-10-02 12:45:00 -0500

    Added city lookup

--- world.js
+++ world.js
@@ -14,3 +14,20 @@ $("#lookup").on("click", function(element) {
         }
     });
 });
+
+$("#lookupCities").on("click", function(element) {
+    element.preventDefault();
+
+    var country = $("#country").val();
+    $.ajax({
+        url: "world.php",
+        type: "GET",
+        data: { country: country, lookup: "cities" },
+        success: function(data) {
+            $("#result").html(data);
+        },
+        error: function() {
+            $("#result").html("An error occurred while fetching data.");
+        }
+    });
+});
--- world.php
+++ world.php
@@ -1,6 +1,7 @@
 <?php
 
 $country = $_GET['country'] ?? '';
+$lookup = $_GET['lookup'] ?? '';
 
 $host = 'localhost';
 $username = 'lab5_user';
@@ -8,24 +9,43 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<table>
-  <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence Year</th>
-    <th>Head of State</th>
-  </tr>
-<?php foreach ($results as $row): ?>
-  <tr>
-    <td><?php echo htmlspecialchars($row['name']); ?></td>
-    <td><?php echo htmlspecialchars($row['continent']); ?></td>
-    <td><?php echo htmlspecialchars($row['independence_year']); ?></td>
-    <td><?php echo htmlspecialchars($row['head_of_state']); ?></td>
-  </tr>
-<?php endforeach; ?>
-</table>
+
+<?php if ($lookup === ''): ?>
+  <?php $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'"); ?>
+  <?php $results = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
+
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?php echo htmlspecialchars($row['name']); ?></td>
+      <td><?php echo htmlspecialchars($row['continent']); ?></td>
+      <td><?php echo htmlspecialchars($row['independence_year']); ?></td>
+      <td><?php echo htmlspecialchars($row['head_of_state']); ?></td>
+    </tr>
+  <?php endforeach; ?>
+  </table>
+<?php elseif ($lookup === 'cities'): ?>
+  <?php $stmt = $conn->query("SELECT * FROM cities WHERE country_code IN (SELECT code FROM countries WHERE name = '$country')"); ?>
+  <?php $results = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?php echo htmlspecialchars($row['name']); ?></td>
+        <td><?php echo htmlspecialchars($row['district']); ?></td>
+        <td><?php echo htmlspecialchars($row['population']); ?></td>
+      </tr>
+    <?php endforeach; ?>
+<?php endif; ?>
