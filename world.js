664f5724a5976e0d52cb6f009518bd5363e45c5a
JustSomeGuy420 <alwynsterling@gmail.com>
2025-10-02 12:24:04 -0500

    Added search capabilities and table formatting

---
+++ world.js
@@ -0,0 +1,16 @@
+$("#lookup").on("click", function(element) {
+    element.preventDefault();
+
+    var country = $("#country").val();
+    $.ajax({
+        url: "world.php",
+        type: "GET",
+        data: { country: country },
+        success: function(data) {
+            $("#result").html(data);
+        },
+        error: function() {
+            $("#result").html("An error occurred while fetching data.");
+        }
+    });
+});
--- world.php
+++ world.php
@@ -1,17 +1,31 @@
 <?php
+
+$country = $_GET['country'] ?? '';
+
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <tr>
+    <td><?php echo htmlspecialchars($row['name']); ?></td>
+    <td><?php echo htmlspecialchars($row['continent']); ?></td>
+    <td><?php echo htmlspecialchars($row['independence_year']); ?></td>
+    <td><?php echo htmlspecialchars($row['head_of_state']); ?></td>
+  </tr>
 <?php endforeach; ?>
-</ul>
+</table>
